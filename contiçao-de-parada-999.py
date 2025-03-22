417a31878ffd9ef70d0b7885a1a6b56af2a42c03
vinicius-coimbra <vctellesfigueiredo@gmail.com>
2025-03-22 15:23:45 -0300

    contiçao-999

---
+++ contiçao-de-parada-999.py
@@ -0,0 +1,10 @@
+n1 = 0
+v = 0
+r = 0
+while r != 999:
+    r = int(input('escreva um numero'))
+    if r != 999:
+        v +=1
+        n1 += r
+
+print('a soma dos numeros e {}\na guantidade de numeros digitatos e {}'.format(n1,v))
