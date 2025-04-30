fdb9818b82978482e6b429b1115a52d233fc0e1b
vinicius-coimbra <vctellesfigueiredo@gmail.com>
2025-04-29 22:45:37 -0300

    Branch limpa

--- Pa.py
+++
@@ -1,8 +0,0 @@
-termo = int(input('escreva o termo da P.A:'))
-razao = int(input('escreva a razao da P.A:'))
-relogio = 0
-while relogio != 10:
-    print(f'{termo}',end='')
-    print(' >> ' if relogio < 9 else ' fim',end='' )
-    relogio +=1
-    termo += razao
--- sexo-feminino-masculino.py
+++
@@ -1,5 +0,0 @@
-sexo = 'nulo'
-while sexo != 'M' and sexo != 'F':
-    sexo = str(input('gual o seu sexo:')).upper()
-    if sexo != 'M' and sexo != 'F':
-        print('isso da errado fi\nfaz de novo')        
