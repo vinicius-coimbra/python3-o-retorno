66629a99667b58de34af587ddff150ea79c097e4
vinicius-coimbra <vctellesfigueiredo@gmail.com>
2025-07-02 18:56:47 -0300

    verificaçao de falor

---
+++ de_0_a_20.py
@@ -0,0 +1,7 @@
+numeros = 0, 1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 , 18 ,19 ,20
+numero_do_usuario = input('escreva um numero de 0 a 20 ')
+while not numero_do_usuario.isdigit():
+    numero_do_usuario = input('numero invalido digite de novo de 0 a 20:')
+numero_do_usuario = int(numero_do_usuario)
+
+print(numeros.index(numero_do_usuario))
