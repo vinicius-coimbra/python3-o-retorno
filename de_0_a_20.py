eb267d29b47c954d1e17363d4f82eddc4494e010
vinicius-coimbra <vctellesfigueiredo@gmail.com>
2025-07-04 22:38:07 -0300

    verificaçao de valor

--- de_0_a_20.py
+++ de_0_a_20.py
@@ -1,11 +1,11 @@
-numeros = 0, 1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 , 18 ,19 ,20
+numeros = 'zero', 'um', 'dois' , 'tres' ,'quadro' ,'cinco' , 'seis' , 'sete' , 'oito' , 'nove' , 'dez' ,'onze' ,'' , 'treze' , 'quadorze' , 'quinze' , 'desezes' , 'desezede' , 'tezoido' ,'dezenove' ,'vinde'
 numero_do_usuario = input('escreva um numero de 0 a 20: ')
 while not numero_do_usuario.isdigit():
     numero_do_usuario = input('numero invalido digite de novo de 0 a 20:')
 numero_do_usuario = int(numero_do_usuario)
-
-if numeros.count(numero_do_usuario):
-    print("tem")
-
+if numero_do_usuario <= 20 and numero_do_usuario >= 0:
+    print(f'voce escolheu o numero {numeros[numero_do_usuario]}')
 else:
-    print('nao tem ')
+    while numero_do_usuario <0 or numero_do_usuario >20:
+        numero_do_usuario = int(input("numero invalido escolha outro entre 0 e 20:"))
+    print(f'voce escolheu o numero {numeros[numero_do_usuario]}')
